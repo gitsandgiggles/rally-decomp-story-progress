b61badfcc235f8dcef79a5c7d0541940655759c7
gitsandgiggles <github@thebubble.co.uk>
2018-09-02 18:13:56 +0100

    Simple card board

--- App.js
+++ App.js
@@ -6,5 +6,46 @@ Ext.define('CustomApp', {
 
         //API Docs: https://help.rallydev.com/apps/2.1/doc/
         console.log('Hello world! I am the decomp stry progress viewer in v0.1 change 2');
+        
+ /* simple card board====       
+        var cardBoardConfig = {
+            xtype: 'rallycardboard',
+            types: ['Defect', 'User Story'],
+            attribute: 'ScheduleState',
+        };
+        this.cardBoard = this.add(cardBoardConfig);   
     }
+*/
+
+// grouped card board====     
+          this.add({
+                        xtype: 'rallycardboard',
+                        types: ['User Story'],
+                        attribute: 'Iteration',
+                        context: this.getContext(),
+                        rowConfig: {
+                            field: 'PortfolioItem'
+                        },
+                        cardConfig: {
+                            fields: [
+                                      'Name',
+                                      'ScheduleState'
+                            ]
+                        }
+                    });
+                }
+                
+//to do:
+
+// change column to iteration - done :)
+
+// change row to parent hierarchy - done-ish - can have parent or Epic but  not  both. (Parent or PortfolioItem)
+
+// indicate state - done :)
+// indicate size and progress vs tasks
+// navigate from current project only, up to epics, then select anything from other squads as well. Mark somehow
+ 
+
 });
+
+
